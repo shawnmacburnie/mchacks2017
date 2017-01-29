c8dd7f7620455a180f947cadc7a553cb13e4a152
Jacob Simmons <jacob.s149@yahoo.com>
2017-01-29 11:57:30 -0500

    a

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ var getLogic = require('./getLogic');
     // var message = "Completed " + l.action.replace(/([A-Z])/g, ' $1');
     // console.log(message);
 // });
-// app.listen(3000, function () {
-// 	  console.log('Example app listening on port 3000!')
-// })
+ app.listen(3000, function () {
+ 	  console.log('Example app listening on port 3000!')
+ })
